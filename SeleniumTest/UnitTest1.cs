21cc2f5f0cf267904e3818e2a794911fcc6b0059
Kombi92 <92.konrad@gmail.com>
2016-04-25 01:42:17 +0200

    Selenium Tests

---
+++ SeleniumTest/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        static IWebDriver driver;
+
+        [AssemblyInitialize]
+        public static void SetUp(TestContext context)
+        {
+            driver = new ChromeDriver(@"C:\Users\Konrad\Source\Repos\Projekt\SeleniumTest");
+        }
+        [TestMethod]
+        public void TestAbout()
+        { 
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+            driver.Navigate().GoToUrl("http://localhost:41340/");
+            driver.FindElement(By.CssSelector("a[href*='/Home/About']")).Click();
+            driver.FindElement(By.TagName("h2")).Click();
+            driver.Navigate().Back();
+        }
+
+
+        [TestMethod]
+        public void TestRegister()
+        {
+            driver.Navigate().GoToUrl("http://localhost:41340/");
+            driver.FindElement(By.Id("registerLink")).Click();
+            driver.FindElement(By.Id("Email")).SendKeys("admin@admin.pl");
+            driver.FindElement(By.Id("Password")).SendKeys("adminadmin");
+            driver.FindElement(By.Id("Password")).SendKeys(Keys.Enter);
+        }
+
+        [TestMethod]
+        public void TestUczen()
+        {
+            driver.Navigate().GoToUrl("http://localhost:41340/");
+            driver.FindElement(By.CssSelector("a[href*='/Uczen']")).Click();
+            driver.FindElement(By.CssSelector("a[href*='/DodajUczen']")).Click();
+        }
+    }
+}
